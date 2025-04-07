f4173702ce828ac2dedaa20619d37785e68bf49c
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-07 15:28:21 +0200

    palindromi

---
+++ palindromi/jv/script.js
@@ -0,0 +1,16 @@
+// creiamo prompt per chiedere parola
+
+const userWard = prompt("inserisci un parola");
+
+// funzione di reverse tramite ciclo per stabilire se la parola è palindroma attraverso un risultato booleano
+const WardUser = userWard;
+function wardRevers(WardUser) {
+  for (i = WardUser.length - 1; i >= 0; --i) {
+    const currentward = WardUser[i];
+
+    console.log(currentward);
+  }
+  return currentward;
+}
+
+console.log(currentward);
