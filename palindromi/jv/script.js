37d8d53a85ca6718483e59777275a2d6f5376c3c
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-07 16:01:38 +0200

    currentpoblem

--- palindromi/jv/script.js
+++ palindromi/jv/script.js
@@ -1,33 +1,31 @@
 // creiamo prompt per chiedere parola
 
-const userWard = prompt("inserisci un parola");
+const userWord = prompt("inserisci un parola");
 
 // funzione di reverse tramite ciclo per stabilire se la parola è palindroma attraverso un risultato booleano
 // ciclo per leggere la parola
-function wardLine(nameLine) {
-  let currentward;
+function wordLine(nameLine) {
   for (i = 0; i < nameLine.length; i++) {
-    const currentward = nameLine[i];
+    const currentWord = nameLine[i];
   }
-  return currentward;
+  return currentWord;
 }
-const Ward = wardLine(userWard);
-console.log(Ward);
+const Word = wordLine(userWord);
+console.log(Word);
 
 // ciclo per leggere al contrario la parola
-function wardRevers() {
-  let currentward;
-  for (i = userWard.length - 1; i >= 0; --i) {
-    const currentward = userWard[i];
+function wordRevers() {
+  for (i = userWord.length - 1; i >= 0; --i) {
+    const currentword = userWord[i];
   }
-  return currentward;
+  return currentword;
 }
-const reversWard = wardRevers();
-console.log(reversWard);
+const reversWord = wordRevers();
+console.log(reversWord);
 
 // validazione
 
-if (wardLine === wardRevers) {
+if (wordLine === wordRevers) {
   alert("la parola è palindroma");
 } else {
   alert(" la parola non è palindroma");
