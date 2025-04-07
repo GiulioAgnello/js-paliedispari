f7c9556903ed1c2993c112d7b2ac12f7612c803d
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-07 15:42:43 +0200

    lettore

--- palindromi/jv/script.js
+++ palindromi/jv/script.js
@@ -3,14 +3,24 @@
 const userWard = prompt("inserisci un parola");
 
 // funzione di reverse tramite ciclo per stabilire se la parola è palindroma attraverso un risultato booleano
-const WardUser = userWard;
-function wardRevers(WardUser) {
-  for (i = WardUser.length - 1; i >= 0; --i) {
-    const currentward = WardUser[i];
-
-    console.log(currentward);
+// ciclo per leggere la parola
+function wardLine() {
+  let currentward;
+  for (i = 0; i < userWard.length; i++) {
+    const currentward = userWard[i];
   }
   return currentward;
 }
+const Ward = wardLine();
+console.log(Ward);
 
-console.log(currentward);
+// ciclo per leggere al contrario la parola
+function wardRevers() {
+  let currentward;
+  for (i = userWard.length - 1; i >= 0; --i) {
+    const currentward = userWard[i];
+  }
+  return currentward;
+}
+const reversWard = wardRevers();
+console.log(reversWard);
